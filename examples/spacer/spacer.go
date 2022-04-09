905118d26f1a099a71d664f2c93c239477d73f57
soypat <graded.sp@gmail.com>
2022-04-09 18:10:06 -0300

    perfect the spacer equation

--- examples/spacer/spacer.go
+++ examples/spacer/spacer.go
@@ -1,22 +1,33 @@
 package main
 
 import (
+	"fmt"
+	"math"
+
 	"github.com/deadsy/sdfx/obj"
 	"github.com/deadsy/sdfx/render"
 	"github.com/deadsy/sdfx/sdf"
 )
 
 func main() {
-	holeDiam := 5.0 // mm
-
-	ftfDiam := holeDiam * 1.8
-	spacerHeight := 27.0 // mm
-	sp, err := obj.HexHead3D(ftfDiam/2, spacerHeight, "")
+	const (
+		spacerHeight     float64 = 5 // mm
+		holeDiam         float64 = 3 // mm
+		plaHoleCorrected         = holeDiam*1.05 + .4
+	)
+	ftf := math.Ceil(plaHoleCorrected*1.4) - .15 // Face to face hex distance
+	hexRadius := getHexRadiusFromFTF(ftf)
+	fmt.Printf("FTF:%.2g, holeCorrected:%.1g\n", ftf, plaHoleCorrected)
+	sp, err := obj.HexHead3D(hexRadius, spacerHeight, "")
 	must(err)
-	hole, err := sdf.Cylinder3D(spacerHeight, holeDiam/2, 0)
+	hole, err := sdf.Cylinder3D(spacerHeight, plaHoleCorrected/2, 0)
 	must(err)
 	sp = sdf.Difference3D(sp, hole)
-	render.RenderSTLSlow(sp, 200, "spacer.stl")
+	render.RenderSTLSlow(sp, 100, fmt.Sprintf("spacer%gx%g.stl", math.Round(holeDiam), math.Round(spacerHeight)))
+}
+
+func getHexRadiusFromFTF(ftf float64) (radius float64) {
+	return ftf / math.Cos(30.*math.Pi/180.) / 2
 }
 
 func must(err error) {
